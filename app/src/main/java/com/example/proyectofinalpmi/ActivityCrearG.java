6901efaa4668aec0cf7a93eb75009457d539d902
noelh55 <hnoel1149@gmail.com>
2023-11-04 23:17:24 -0600

    04/11/23

---
+++ app/src/main/java/com/example/proyectofinalpmi/ActivityCrearG.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinalpmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityCrearG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_g);
+    }
+}
