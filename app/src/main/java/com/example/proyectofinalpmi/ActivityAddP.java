2620d5eaddf91d6be1a206e35a737579510f5a6f
noelh55 <hnoel1149@gmail.com>
2023-11-05 21:39:03 -0600

    05/11/23

--- app/src/main/java/com/example/proyectofinalpmi/ActivityAddP.java
+++ app/src/main/java/com/example/proyectofinalpmi/ActivityAddP.java
@@ -3,9 +3,11 @@ package com.example.proyectofinalpmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ActivityAddP extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
